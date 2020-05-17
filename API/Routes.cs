43e2c638a028e9fdb23f5284a0d5c74b768fa4ca
Mani0811 <mani.ciem@gmail.com>
2020-05-17 20:02:24 +0530

    modifed

--- API/Routes.cs
+++ API/Routes.cs
@@ -4,21 +4,21 @@ namespace Evento.Service
 {
     public static class Routes
     {
-        public const string Evento = "event";
+        public const string Evento = "/evento";
 
-        public const string DeleteEvento = "event/{eventId}";
+        public const string DeleteEvento = "evento/{eventId}";
 
-        public const string LeaveEvento = "event/{eventId}/participant/{participantId}/leave";
+        public const string LeaveEvento = "evento/{eventId}/participant/{participantId}/leave";
 
-        public const string EndEvento = "event/{eventId}/end";
+        public const string EndEvento = "evento/{eventId}/end";
 
-        public const string ExtendEvento = "event/{eventId}/extend/{endTime}";
+        public const string ExtendEvento = "evento/{eventId}/extend/{endTime}";
 
-        public const string EventoParticipants = "event/{eventId}/participants";
+        public const string EventoParticipants = "evento/{eventId}/participants";
 
-        public const string EventoParticipantState = "event/{eventId}/participant/{participantId}/state/{state}";
+        public const string EventoParticipantState = "evento/{eventId}/participant/{participantId}/state/{state}";
 
-        public const string ServiceStatus = "event/service-status";
+        public const string ServiceStatus = "evento/service-status";
 
     }
 }
